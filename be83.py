e5781ce802ffbc2efabb95ae170f169757571908
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2017-12-30 14:45:41 +0530

    Create be83.py

---
+++ be83.py
@@ -0,0 +1,17 @@
+ def main():
+	l=[]
+	while(True):
+		s=input()
+		try:
+			if '/' in s:
+				a,b = map(int,s.split('/'))
+				r=a/b
+			else :
+				a,b=map(int,s.split('%'))
+				r=a%b
+			l.append(int(r))
+			r=[]
+		except ValueError:
+			break
+	for i in l:
+		print(i)
