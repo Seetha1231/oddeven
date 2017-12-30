0e5ac6f6ca7a475be0158208ff98e005b51389d6
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2017-12-30 12:00:26 +0530

    Create be75.py

---
+++ be75.py
@@ -0,0 +1,18 @@
+ def midchenge():
+	s=input()
+	n=len(s)
+	l=list(s)
+	s=''
+	if n%2!=0:
+		for i in range(n//2):
+			s+=l[i]
+		s+='*'
+		j=n//2+1
+	else:
+		for i in range(n//2-1):
+			s+=l[i]
+		s+='**'
+		j=n//2+1
+	for i in range(j,n):
+		s+=l[i]
+	print(s)
